4596713de5695830f9c847bfcbdb4022790ce3f7
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-03-13 16:57:48 -0300

    :rocket: [Req. 05] - Mensagem de acerto ou erro

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 const divRGBColor = document.getElementById('rgb-color');
 const divBallOption = document.getElementById('ball-options');
+const answer = document.getElementById('answer');
 
 const color = () => Math.ceil(Math.random() * 255);
+let colorSelected = '';
 
 const showRGBonPage = () => {
-  divRGBColor.innerHTML = `(${color()}, ${color()}, ${color()})`;
+  colorSelected = `(${color()}, ${color()}, ${color()})`;
+  divRGBColor.innerHTML = colorSelected;
+};
+
+const clearSelected = () => {
+  for (let index = 0; index < divBallOption.children.length; index += 1) {
+    divBallOption.children[index].setAttribute('class', 'ball');
+  }
+};
+
+const checkAnswer = (e) => {
+  clearSelected();
+  e.target.className = 'ball selected';
+  if (e.target.style.backgroundColor === `rgb${colorSelected}`) {
+    answer.innerHTML = '<strong>Acertou!</strong>';
+  } else {
+    answer.innerHTML = '<strong>Errou! Tente novamente!</strong>';
+  }
 };
 
 const generateOptions = (num) => {
+  const positionAnswer = Math.ceil(Math.random() * 6);
   for (let index = 0; index < num; index += 1) {
     const newEl = document.createElement('div');
     newEl.className = 'ball';
-    newEl.style.backgroundColor = `rgb(${color()}, ${color()}, ${color()})`;
+    newEl.style.backgroundColor = positionAnswer === index
+      ? `rgb${colorSelected}`
+      : `rgb(${color()}, ${color()}, ${color()})`;
+    newEl.addEventListener('click', checkAnswer);
     divBallOption.appendChild(newEl);
   }
 };
