2b12bdd404dd507413bf5ffed082fb0da9589b0d
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-03-13 16:09:30 -0300

    :rocket: [Req. 03] - Gerar opções de cores

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+const divRGBColor = document.getElementById('rgb-color');
+const divBallOption = document.getElementById('ball-options');
+
 const color = () => Math.ceil(Math.random() * 255);
 
-function showRGBonPage() {
-  const divRGB = document.getElementById('rgb-color');
-  divRGB.innerHTML = `(${color()}, ${color()}, ${color()})`;
-}
+const showRGBonPage = () => {
+  divRGBColor.innerHTML = `(${color()}, ${color()}, ${color()})`;
+};
+
+const generateOptions = (num) => {
+  for (let index = 0; index < num; index += 1) {
+    const newEl = document.createElement('div');
+    newEl.className = 'ball';
+    newEl.style.backgroundColor = `rgb(${color()}, ${color()}, ${color()})`;
+    divBallOption.appendChild(newEl);
+  }
+};
 
 window.onload = () => {
   showRGBonPage();
+  generateOptions(6);
 };
